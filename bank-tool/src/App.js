bb6ce079edd349f1d7799400e16452640ff1bb78
Kyle Chinick <kylechinick@icloud.com>
2022-09-16 12:03:51 -0700

    Initial commit for internal test bank tool

---
+++ bank-tool/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <CurrencyRow />
+      <div>=</div>
+      <CurrencyRow />
+    </>
+  );
+}
+
+export default App;
