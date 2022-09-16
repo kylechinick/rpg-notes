182a407d3252d06caf8daf5c0facb4e8a7ed82a8
Kyle Chinick <kylechinick@icloud.com>
2022-09-16 16:54:13 -0700

    Add initial currency code - testing only

--- bank-tool/src/App.js
+++ bank-tool/src/App.js
@@ -1,7 +1,18 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL = 'https://api.apilayer.com/fixer/latest';
+
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <>
       <h1>Hello World</h1>
