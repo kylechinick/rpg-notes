c7132c25c0e64b856f7285697d4df2aa56c2167c
Kyle Chinick <kylechinick@icloud.com>
2022-09-23 14:26:23 -0700

    WIP Switch to simplified form approach for testing

--- bank-tool/src/App.js
+++ bank-tool/src/App.js
@@ -1,112 +1,30 @@
-import React, { useReducer, useState } from 'react';
-import './App.css';
-
-const formReducer = (state, event) => {
-  if (event.reset) {
-    return {
-      apple: '',
-      count: 0,
-      name: '',
-      'gift-wrap': false
-    };
-  }
-  return {
-    ...state,
-    [event.name]: event.value
-  };
-};
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
 function App() {
-  const [formData, setFormData] = useReducer(formReducer, {
-    count: 100
-  });
-  const [submitting, setSubmitting] = useState(false);
+  const [name, setName] = useState("");
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setSubmitting(true);
-
-    setTimeout(() => {
-      setSubmitting(false);
-      setFormData({
-        reset: true
-      });
-    }, 6000);
-  };
-
-  const handleChange = event => {
-    const isCheckbox = event.target.type === 'checkbox';
-    setFormData({
-      name: event.target.name,
-      value: isCheckbox ? event.target.checked : event.target.value
-    });
-  };
+    alert(`The name you entered was: ${name}`)
+  }
 
   return (
-    <div className='wrapper'>
-      <h1>How About Them Apples</h1>
-      {submitting && (
-        <div>
-          You are submitting the following:
-          <ul>
-            {Object.entries(formData).map(([name, value]) => (
-              <li key={name}>
-                <strong>{name}</strong>: {value.toString()}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <fieldset disabled={submitting}>
-          <label>
-            <p>Name</p>
-            <input
-              name='name'
-              onChange={handleChange}
-              value={formData.name || ''}
-            />
-          </label>
-        </fieldset>
-        <fieldset disabled={submitting}>
-          <label>
-            <p>Apples</p>
-            <select
-              name='apple'
-              onChange={handleChange}
-              value={formData.apple || ''}>
-              <option value=''>--Please choose an option--</option>
-              <option value='fuji'>Fuji</option>
-              <option value='jonathan'>Jonathan</option>
-              <option value='honey-crisp'>Honey Crisp</option>
-            </select>
-          </label>
-          <label>
-            <p>Count</p>
-            <input
-              type='number'
-              name='count'
-              onChange={handleChange}
-              step='1'
-              value={formData.count || ''}
-            />
-          </label>
-          <label>
-            <p>Gift Wrap</p>
-            <input
-              checked={formData['gift-wrap'] || false}
-              disabled={formData.apple !== 'fuji'}
-              name='gift-wrap'
-              onChange={handleChange}
-              type='checkbox'
-            />
-          </label>
-        </fieldset>
-        <button type='submit' disabled={submitting}>
-          Submit
-        </button>
-      </form>
-    </div>
+    <React.Fragment>
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
+      <div>
+        <p>{name}</p>
+      </div>
+    </React.Fragment>
   );
 }
 
