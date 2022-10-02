ae153d20a1c76073741443d9ac160e90e55aa009
Kyle Chinick <kylechinick@icloud.com>
2022-10-02 13:24:33 -0700

    Install motion framer, WIP add initial success for using framer motion to animate note detail entrance

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const NoteTitle = styled.h1`
   color: red;
@@ -10,14 +11,24 @@ function NoteDetail(props) {
   const { note, onClickingDelete, onClickingEdit } = props;
   return (
     <>
-      <NoteTitle>Note Detail</NoteTitle>
-      <h2>{note.name}</h2>
-      <h3>Entry Type</h3>
-      <p>{note.entryType}</p>
-      <h3>Description</h3>
-      <p>{note.noteText}</p>
-      <button onClick={onClickingEdit}>Update Note</button>
-      <button onClick={() => onClickingDelete(note.id)}>Delete Note</button>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.2,
+          ease: [0, 0.71, 0.2, 1.01]
+        }}
+      >
+        <NoteTitle>Note Detail</NoteTitle>
+        <h2>{note.name}</h2>
+        <h3>Entry Type</h3>
+        <p>{note.entryType}</p>
+        <h3>Description</h3>
+        <p>{note.noteText}</p>
+        <button onClick={onClickingEdit}>Update Note</button>
+        <button onClick={() => onClickingDelete(note.id)}>Delete Note</button>
+      </motion.div>
     </>
   );
 }
