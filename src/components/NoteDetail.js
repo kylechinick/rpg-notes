a798b6c4f395ee0dd6f9eed706109ee02ef0fdb6
Kyle Chinick <kylechinick@icloud.com>
2022-10-02 14:44:23 -0700

    Add button animation that begins at an initial coordinate and ends at DOM/CSS specified location – still WIP for adding exit animations

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -2,33 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+// import { motion, AnimatePresence } from 'framer-motion';
 
 const NoteTitle = styled.h1`
-  color: red;
+  margin-bottom: 1rem;
+  text-transform: uppercase;
+  font-weight: 900;
 `;
 
 function NoteDetail(props) {
   const { note, onClickingDelete, onClickingEdit } = props;
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{
-          duration: 0.8,
-          delay: 0.2,
-          ease: [0, 0.71, 0.2, 1.01]
-        }}
+      <NoteTitle>Note Detail</NoteTitle>
+      <h2>{note.name}</h2>
+      <h3>Entry Type</h3>
+      <p>{note.entryType}</p>
+      <h3>Description</h3>
+      <p>{note.noteText}</p>
+      <motion.button
+        initial={{ x: -100 }}
+        animate={{ x: 0 }}
+        transition={{ ease: 'easeOut', duration: 2 }}
+        onClick={onClickingEdit}
       >
-        <NoteTitle>Note Detail</NoteTitle>
-        <h2>{note.name}</h2>
-        <h3>Entry Type</h3>
-        <p>{note.entryType}</p>
-        <h3>Description</h3>
-        <p>{note.noteText}</p>
-        <button onClick={onClickingEdit}>Update Note</button>
-        <button onClick={() => onClickingDelete(note.id)}>Delete Note</button>
-      </motion.div>
+        Update Note
+      </motion.button>
+      <motion.button
+        initial={{ x: -100 }}
+        animate={{ x: 0 }}
+        transition={{ ease: 'easeOut', duration: 2 }}
+        onClick={() => onClickingDelete(note.id)}
+      >
+        Delete Note
+      </motion.button>
     </>
   );
 }
