efdc4eb687ec525bcea6eee8d74e5d35e5c01460
Kyle Chinick <kylechinick@icloud.com>
2022-10-04 16:41:00 -0700

    WIP commit – add form submission functionality and initial logic to test

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -3,18 +3,39 @@ import React from 'react';
 function CurrencyConverter() {
   function handleCurrencyConverterSubmission(event) {
     event.preventDefault();
+
+    const currencyType = document.querySelector('#currency-type');
+    const currencyAmount = document.querySelector('#currency-amount');
+
+    if (currencyType) {
+      console.log(currencyAmount / 2);
+    }
+
+    // switch (currency-type) {
+    //   case 'silver':
+
+    //     break;
+
+    //   default:
+    //     break;
+    // }
   }
 
   return (
     <React.Fragment>
-      <form>
-        <input type="text" placeholder='currency' />
-        <input type="number" name="currency-amount" />
-
+      <h1>Currency Converter</h1>
+      <p>Convert a currency type to its value in gold</p>
+      <form onSubmit={handleCurrencyConverterSubmission}>
+        <input type='text' placeholder='Currency Type' id='currency-type' />
+        <input
+          type='number'
+          placeholder='Currency Amount'
+          name='currency-amount'
+        />
+        <button>Convert</button>
       </form>
-
     </React.Fragment>
-  )
+  );
 }
 
 export default CurrencyConverter;
