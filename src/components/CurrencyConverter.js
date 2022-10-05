25255a5223314516d11dc680148c04ba6fa59303
Kyle Chinick <kylechinick@icloud.com>
2022-10-05 11:56:37 -0700

    WIP continue adding conversion output support

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,4 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+
+const ConversionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  padding: 1rem;
+`;
+
+const ConversionForm = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 1rem;
+`;
+
+const CurrencyInput = styled.input`
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+`;
 
 function CurrencyConverter() {
   // const [currentCurrencyValue, setcurrentCurrencyValue] = useState(null);
@@ -6,18 +26,21 @@ function CurrencyConverter() {
   function handleCurrencyConverterSubmission(event) {
     event.preventDefault();
 
-    console.log();
+    let convertedCurrencyOutput = document.querySelector(
+      '#currencyAmountOutput'
+    );
+
     const currencyType = event.target.currencyType.value;
     const currencyAmount = event.target.currencyAmount.value;
 
     switch (currencyType) {
       case 'copper':
-
         console.log(
           `Your total amount of ${currencyType} is worth ${
             currencyAmount / 100
           } in gold.`
         );
+        convertedCurrencyOutput.text = currencyAmount;
         break;
       case 'silver':
         console.log(
@@ -54,27 +77,32 @@ function CurrencyConverter() {
   }
 
   return (
-    <React.Fragment>
+    <ConversionContainer>
       <h1>Currency Converter</h1>
       <p>Convert a currency type to its value in gold</p>
-      <form onSubmit={handleCurrencyConverterSubmission}>
-        <input type='text' placeholder='Currency Type' name='currencyType' />
-        <input
+      <ConversionForm onSubmit={handleCurrencyConverterSubmission}>
+        <CurrencyInput
+          type='text'
+          placeholder='Currency Type'
+          name='currencyType'
+        />
+        <CurrencyInput
           type='number'
           placeholder='Currency Amount'
           name='currencyAmount'
+          id='currencyAmount'
         />
         <button>Convert</button>
-      </form>
+      </ConversionForm>
       <div>
         <p>Converted Silver</p>
-        <p id='convertedSilver'>0</p>
+        <p id='currencyAmountOutput'>0</p>
       </div>
       <div>
         <p>Converted Grand Total</p>
         <p id='convertedGrandTotal'>0</p>
       </div>
-    </React.Fragment>
+    </ConversionContainer>
   );
 }
 
